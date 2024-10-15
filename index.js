cb894d429641e0bc159c4be9ebf12613756837d0
pengbo <pbstar@sina.cn>
2024-10-15 20:02:06 +0800

    1

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ import processImgs from './src/process_imgs.js';
 import answer from './src/answer.js';
 
 let monitors = Monitor.all();
-const start = (item) => {
-  let image = item.captureImageSync();
+const start = (win) => {
+  let image = win.captureImageSync();
   let items = [{
-    x: 195,
-    y: 275,
+    x: 192,
+    y: 270,
     width: 50,
-    height: 30,
+    height: 40,
     text: ''
   }, {
     x: 310,
-    y: 275,
+    y: 270,
     width: 50,
-    height: 30,
+    height: 40,
     text: ''
   }];
   let ans = {
@@ -30,21 +30,28 @@ const start = (item) => {
   processImgs(image, items).then(res => {
     let res1 = res[0].text;
     let res2 = res[1].text;
-    // 判断结果是否为数字
-    if (!res1.match(/^[0-9]+$/) || !res2.match(/^[0-9]+$/)) {
+    console.log('res1:' + res1 + ';res2:' + res2);
+    // 判断结果是否包含数字
+    const regex = /\d/;
+    if (!regex.test(res1) || !regex.test(res2)) {
+      setTimeout(() => {
+        start(win);
+      }, 2000);
       return;
     }
     res1 = parseInt(res1);
     res2 = parseInt(res2);
     if (res1 > res2) ans.text = '>';
     else if (res1 < res2) ans.text = '<';
-    console.log('res1:' + res1 + ';res2:' + res2 + ';ans:' + ans.text);
-    answer(ans)
+    answer(ans).then(() => {
+      console.log('res1:' + res1 + ';res2:' + res2 + ';ans:' + ans.text);
+      setTimeout(() => {
+        start(win);
+      }, 500);
+    })
   });
 }
 if (monitors.length > 0) {
   console.log('-----------初始化完毕------------')
-  setInterval(() => {
-    start(monitors[0]);
-  }, 1200);
+  start(monitors[0]);
 }
--- src/answer.js
+++ src/answer.js
@@ -20,29 +20,33 @@ const drow = (sx, sy, x, y) => {
 }
 
 export default function answer(ans) {
-  // 鼠标当前位置
-  let mouseX = ans.x;
-  let mouseY = ans.y;
-  // 移动鼠标到绘制起点
-  robot.moveMouse(mouseX, mouseY);
-  // 模拟鼠标左键按下（开始绘制）
-  robot.mouseToggle("down");
-  // 绘制
-  if (ans.text == '<') {
-    mouseX = mouseX - 30
-    mouseY = mouseY + 30
-    drow(mouseX + 30, mouseY - 30, mouseX, mouseY);
-    mouseX = mouseX + 30
-    mouseY = mouseY + 30
-    drow(mouseX - 30, mouseY - 30, mouseX, mouseY);
-  } else if (ans.text == '>') {
-    mouseX = mouseX + 30
-    mouseY = mouseY + 30
-    drow(mouseX - 30, mouseY - 30, mouseX, mouseY);
-    mouseX = mouseX - 30
-    mouseY = mouseY + 30
-    drow(mouseX + 30, mouseY - 30, mouseX, mouseY);
-  }
-  // 模拟鼠标左键抬起
-  robot.mouseToggle("up");
+  return new Promise((resolve, reject) => {
+    // 鼠标当前位置
+    let mouseX = ans.x;
+    let mouseY = ans.y;
+    // 移动鼠标到绘制起点
+    robot.moveMouse(mouseX, mouseY);
+    // 模拟鼠标左键按下（开始绘制）
+    robot.mouseToggle("down");
+    // 绘制
+    if (ans.text == '<') {
+      mouseX = mouseX - 30
+      mouseY = mouseY + 30
+      drow(mouseX + 30, mouseY - 30, mouseX, mouseY);
+      mouseX = mouseX + 30
+      mouseY = mouseY + 30
+      drow(mouseX - 30, mouseY - 30, mouseX, mouseY);
+    } else if (ans.text == '>') {
+      mouseX = mouseX + 30
+      mouseY = mouseY + 30
+      drow(mouseX - 30, mouseY - 30, mouseX, mouseY);
+      mouseX = mouseX - 30
+      mouseY = mouseY + 30
+      drow(mouseX + 30, mouseY - 30, mouseX, mouseY);
+    }
+    // 模拟鼠标左键抬起
+    robot.mouseToggle("up");
+    resolve();
+  });
+
 }
